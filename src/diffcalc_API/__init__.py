9c5b586bd487ff240d26627a8e0cac130cdaa34d
Rose Yemelyanova <rose.yemelyanova@diamond.ac.uk>
2022-06-28 17:04:04 +0100

    added FastAPI which uses pickling for persistence to calculate UB matrix

--- src/diffcalc_API/__init__.py
+++ src/diffcalc_API/__init__.py
@@ -3,4 +3,4 @@ from ._version_git import __version__
 
 # __all__ defines the public API for the package.
 # Each module also defines its own __all__.
-__all__ = ["__version__", "hello"]
+__all__ = ["__version__", "hello", "server"]
