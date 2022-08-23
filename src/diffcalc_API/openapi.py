6c4487eaad6df53860c2e66f19d247b0c3cdd8e7
Rose Yemelyanova <rose.yemelyanova@diamond.ac.uk>
2022-08-23 13:15:14 +0000

    modified the openapi.py script so that openapi.json isnt created by virtue of just being imported

--- src/diffcalc_API/openapi.py
+++ src/diffcalc_API/openapi.py
@@ -4,14 +4,15 @@ from fastapi.openapi.utils import get_openapi
 
 from diffcalc_API.server import app
 
-with open("openapi.json", "w") as f:
-    json.dump(
-        get_openapi(
-            title=app.title,
-            version=app.version,
-            openapi_version=app.openapi_version,
-            description=app.description,
-            routes=app.routes,
-        ),
-        f,
-    )
+if __name__ == "__main__":
+    with open("openapi.json", "w") as f:
+        json.dump(
+            get_openapi(
+                title=app.title,
+                version=app.version,
+                openapi_version=app.openapi_version,
+                description=app.description,
+                routes=app.routes,
+            ),
+            f,
+        )
