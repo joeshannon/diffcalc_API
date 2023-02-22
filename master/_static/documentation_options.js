70378bd7e2bfa6c3fa19def96d96d1c2d640893e
RAYemelyanova <RAYemelyanova@users.noreply.github.com>
2023-02-22 17:05:58 +0000

    deploy: 6e80bb03791026e7b60f23c2d6cfbb82edbf8b55

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,14 +1,14 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0+untagged.gd9e71b7',
-    LANGUAGE: 'None',
+    VERSION: '0.4.2.dev1+g6e80bb0.d20230222',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false,
+    NAVIGATION_WITH_KEYS: true,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
