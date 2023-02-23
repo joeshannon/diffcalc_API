f4af88ecd255e9824929e320428f4ab219dc1520
Rose Yemelyanova <rose.yemelyanova@diamond.ac.uk>
2023-02-23 13:59:27 +0000

    added host=0.0.0.0 to the uvicorn run command so that any container running an image of this will accept traffic from other machines

--- src/diffcalc_api/__main__.py
+++ src/diffcalc_api/__main__.py
@@ -13,7 +13,7 @@ def main(args=None):
     parser = ArgumentParser()
     parser.add_argument("--version", action="version", version=__version__)
     args = parser.parse_args(args)
-    run("diffcalc_api.server:app")
+    run("diffcalc_api.server:app", host="0.0.0.0")
 
 
 if __name__ == "__main__":
